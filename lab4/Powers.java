0f8b648193297f164ee395b3e19921094c96ef85
MrInterBugs <Aedan@mrinterbugs.uk>
2019-10-24 14:59:29 +0100

    Finished Checkpoint 4

--- lab4/Powers.java
+++ lab4/Powers.java
@@ -2,24 +2,37 @@ import java.util.Scanner;
 
 public class Powers {
 	public static void main(String[] args) {
+
 		System.out.print("Please enter a number to see if it makes a perfect square or cube: ");
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
 		perfectSquare(n);
 		perfectCube(n);
 	}
+
 	static void perfectSquare(int n) {
+		boolean noans = true;
 		for(int i=0; i<n; i++) {
 			if(n == i*i) {
-				System.out.println("Perfect Square.");
+				System.out.println("It is a perfect square.");
+				noans = false;
 			}
 		}
+		if(noans) {
+			System.out.println("It is not a perfect square.");
+		}
 	}
+
 	static void perfectCube(int n) {
+		boolean noans = true;
 		for(int i=0; i<n; i++) {
                         if(n == i*i*i) {
-                                System.out.println("Perfect Cube.");
+                                System.out.println("it is a perfect cube.");
+																noans = false;
 			}
-                }
+    }
+		if(noans) {
+			System.out.println("It is not a perfect cube.");
+		}
 	}
 }
