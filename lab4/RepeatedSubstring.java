37dd679608a6bf21e56c83a6a51ad63897095eb4
MrInterBugs <aedan@mrinterbugs.uk>
2019-10-31 12:40:37 +0000

    First Commit From IntelliJ.

---
+++ lab4/RepeatedSubstring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class RepeatedSubstring {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a sentance: ");
+        String s1 = input.nextLine();
+
+
+    }
+}
